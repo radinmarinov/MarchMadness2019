249c4ed2c21cf185cb11e1563a0be03cac4dbf99
radinmarinov <radinmarinov2021@u.northwestern.edu>
2019-07-13 18:02:35 -0500

    completed tournament simulator including neural net model

--- tourney_simulator.py
+++ tourney_simulator.py
@@ -6,29 +6,14 @@ Created on Thu Jul  4 20:56:37 2019
 """
 import pandas as pd
 import numpy as np
-"""
+import random
 from sklearn.neural_network import MLPClassifier
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import roc_auc_score, precision_score, recall_score, accuracy_score
 
+random.seed(1)
 end_of_season_stats_df              = pd.DataFrame.from_csv("C:/Users/Radin/Documents/MarchMadness2019/end_of_season_stats.csv")
 tourney_matchup_df = pd.DataFrame.from_csv("C:/Users/Radin/Documents/MarchMadness2019/tourney_matchup_df.csv")
-stats = list(tourney_matchup_df)[4:len(list(tourney_matchup_df))]
-X_train, X_test, y_train, y_test = train_test_split(tourney_matchup_df[stats], tourney_matchup_df['Team1_won'])
-scaler = StandardScaler()
-scaler.fit(X_train)
-X_train = scaler.transform(X_train)
-X_test = scaler.transform(X_test)
-mlp = MLPClassifier(hidden_layer_sizes=(2),  solver='sgd', activation='relu')
-mlp.fit(X_train,y_train)
-predictions = mlp.predict(X_test)
-mod_accuracy  = accuracy_score(y_test, predictions)
-mod_auc       = roc_auc_score(y_test, predictions)
-mod_precision = precision_score(y_test, predictions)
-mod_recall    = recall_score(y_test, predictions)
-"""
-end_of_season_stats_df              = pd.DataFrame.from_csv("C:/Users/Radin/Documents/MarchMadness2019/end_of_season_stats.csv")
 #Tourney Simulator
 seeds_df                            = pd.DataFrame.from_csv("C:/Users/Radin/Documents/MarchMadness2019/Stage2DataFiles/NCAATourneySeeds.csv")
 slots_df                            = pd.DataFrame.from_csv("C:/Users/Radin/Documents/MarchMadness2019/Stage2DataFiles/NCAATourneySlots.csv")
@@ -47,7 +32,6 @@ slots_df['Season']                  = slots_df.Season.str[:4]
 slots_df['Season']                  = slots_df['Season'].astype(int)
 slots_df                            = slots_df[slots_df.Season > 2002]
 
-
 seeds_df              = seeds_df.rename(index = str, columns={'Seed' : 'StrongSeed'})
 slots_df                  = pd.merge(slots_df, seeds_df, how='left', on= ['Season','StrongSeed'])
 seeds_df              = seeds_df.rename(index = str, columns={'StrongSeed' : 'WeakSeed'})
@@ -61,9 +45,20 @@ conferences_df['Season']            = conferences_df['Season'].apply(str)
 conferences_df['Season']            = conferences_df.Season.str[:4]
 conferences_df['Season']            = conferences_df['Season'].astype(int)
 conferences_df                      = conferences_df[conferences_df.Season > 2002]
+teams_df                            = pd.DataFrame.from_csv("C:/Users/Radin/Documents/MarchMadness2019/Stage2DataFiles/Teams.csv")
+teams_df = teams_df.reset_index()
+def get_model(year):
+    stats = list(tourney_matchup_df)[4:len(list(tourney_matchup_df))]
+    X_train = tourney_matchup_df[tourney_matchup_df.Season < year][stats]
+    y_train = tourney_matchup_df[tourney_matchup_df.Season < year]['Team1_won']
+    scaler = StandardScaler()
+    scaler.fit(X_train)
+    X_train = scaler.transform(X_train)
+    mlp = MLPClassifier(hidden_layer_sizes=(2,5,3),  solver='sgd', activation='identity', random_state=1)
+    mlp.fit(X_train,y_train)
+    return [mlp, scaler]
 
-def get_winner(team1, team2, year):
-    
+def create_matchup(team1, team2, year, conferences_df):
     team1_stats = end_of_season_stats_df[(end_of_season_stats_df.Season == year) & (end_of_season_stats_df.teamID == team1)]
     team2_stats = end_of_season_stats_df[(end_of_season_stats_df.Season == year) & (end_of_season_stats_df.teamID == team2)]
     for stat in list(end_of_season_stats_df):
@@ -75,35 +70,69 @@ def get_winner(team1, team2, year):
     team2_seed = int(team2_seed[1:3])
     team1_stats = team1_stats.drop('Team1_Season', axis=1)
     team2_stats = team2_stats.drop('Team2_Season', axis=1)
-    matchup1 = pd.concat([team1_stats, team2_stats], axis=1, sort=False)
-    matchup1['Team1_seed'] = team1_seed
-    matchup1['Team2_seed'] = team2_seed
+    matchup = pd.concat([team1_stats, team2_stats], axis=1, sort=False)
+    matchup['Team1_seed'] = team1_seed
+    matchup['Team2_seed'] = team2_seed
     for stat in list(end_of_season_stats_df)[2:len(list(end_of_season_stats_df))]:
-        matchup1['diff_' + stat] = matchup1['Team1_' + stat] - matchup1['Team2_' + stat]
-    matchup1['diff_seed'] = team1_seed - team2_seed
+        matchup['diff_' + stat] = matchup['Team1_' + stat] - matchup['Team2_' + stat]
+    matchup['diff_seed'] = team1_seed - team2_seed
     
-    matchup1['Season'] = year
-    conferences_df            = conferences_df.rename(index= str, columns={'TeamID':'Team1_teamID'})
-    matchup1                  = pd.merge(matchup1, conferences_df, how= 'left', on= ['Season', 'Team1_teamID'])
-    conferences_df            = conferences_df.rename(index= str, columns={'Team1_teamID':'Team2_teamID'})
-    matchup1                  = pd.merge(matchup1, conferences_df, how= 'left', on= ['Season', 'Team2_teamID'])
-    matchup1 = matchup1.drop('Season', axis=1)
-    matchup1 = pd.get_dummies(matchup1)
-
-    missing_cols = list(set(tourney_matchup_df) - set(matchup1 ))
+    matchup['Season'] = year
+    conferences_df = conferences_df.rename(columns={ conferences_df.columns[1]: "Team1_teamID" })
+    matchup                  = pd.merge(matchup, conferences_df, how= 'left', on= ['Season', 'Team1_teamID'])
+    conferences_df = conferences_df.rename(columns={ conferences_df.columns[1]: "Team2_teamID" })
+    matchup                  = pd.merge(matchup, conferences_df, how= 'left', on= ['Season', 'Team2_teamID'])
+    matchup = matchup.drop('Season', axis=1)
+    matchup = pd.get_dummies(matchup)
+    missing_cols = list(set(tourney_matchup_df) - set(matchup ))
     missing_cols.remove('Team1_won')
     for col in missing_cols:
-        matchup1[col] = 0
-    
-    return team1
+        matchup[col] = 0
+        
+    return matchup
 
+def get_winner(team1, team2, year, model):
+    matchup1 = create_matchup(team1, team2, year, conferences_df)
+    matchup2 = create_matchup(team2, team1, year, conferences_df)
+    
+    stats = list(tourney_matchup_df)[4:len(list(tourney_matchup_df))]
+
+    mlp= model[0]
+    scaler = model[1]
+    X_test1 = matchup1[stats]
+    X_test1 = scaler.transform(X_test1)
+    predictions1 = mlp.predict(X_test1)
+    probabilities_mlp1 = mlp.predict_proba(X_test1)
+    
+    X_test2 = matchup2[stats]
+    X_test2 = scaler.transform(X_test2)
+    predictions2 = mlp.predict(X_test2)
+    probabilities_mlp2 = mlp.predict_proba(X_test2)
+    
+    if predictions1 == predictions2:
+        if probabilities_mlp1[0][1] > probabilities_mlp2[0][1]:
+            return team1
+        if probabilities_mlp2[0][1] > probabilities_mlp1[0][1]:
+            return team2
+        else:
+            if matchup1.diff_seed.iloc[0] < 0:
+                return team1
+            else:
+                return team2
+            
+    if predictions1 ==1:
+        return team1
+    else: 
+        return team2
+    
 def season_simulator(year):
     season = slots_df[slots_df.Season == year]
     winners = {}
+    model = get_model(year)
     for slot in season.Slot[~np.isnan(season[['TeamID_x', 'TeamID_y']]).any(1)]:
         team1 = season.TeamID_x[season.Slot == slot].iloc[0]
         team2 = season.TeamID_y[season.Slot == slot].iloc[0]
-        winners[slot] = get_winner(team1, team2, year)
+        winners[slot] = get_winner(team1, team2, year, model)
         season.Winner[season.Slot == slot]  = winners[slot]
     while season[['TeamID_x', 'TeamID_y']].isnull().values.any():   
         for slot in season.Slot:
@@ -115,10 +144,10 @@ def season_simulator(year):
             if np.isnan(team2):                    
                 season.TeamID_y[season.Slot == slot] = winners[season.WeakSeed[season.Slot == slot].iloc[0]]
                 team2 = season.TeamID_y[season.Slot == slot].iloc[0]
-            season.Winner[season.Slot == slot]  = get_winner(team1, team2, year)
+            season.Winner[season.Slot == slot]  = get_winner(team1, team2, year, model)
             winners[slot] = season.Winner[season.Slot == slot].iloc[0]
     return season
                     
                     
-                    
-                    
+test_season = season_simulator(2019)
+test_season = pd.merge(test_season, teams_df, how= 'left', left_on=['Winner'], right_on=['TeamID'])              
